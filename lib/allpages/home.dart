92a8805fa8fd601ecf8b2386fa9e08055c616a5c
Masud322 <masudurrahman5656@gmail.com>
2022-12-10 14:02:33 +0600

    all ok

--- lib/allpages/home.dart
+++ lib/allpages/home.dart
@@ -25,7 +25,7 @@ Future<void> main() async {
 }
 
 class Home_Page extends StatelessWidget {
-  const Home_Page({Key? key}) : super(key: key);
+  const Home_Page( {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
